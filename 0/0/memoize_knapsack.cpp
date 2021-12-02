d9a47b6f625f0bfad94bdcc70877ebdd60d62ce1
Deepti Gupta <91720639+Deepti675@users.noreply.github.com>
2021-12-02 09:40:22 +0530

    Update memoize_knapsack.cpp

--- 0/0/memoize_knapsack.cpp
+++ 0/0/memoize_knapsack.cpp
@@ -52,3 +52,50 @@ int main()
    cout<<knapsack(w,wt,val,n-1,dp);
     return 0;
 }
+
+
+
+/////another way passing table 
+
+
+
+#include <bits/stdc++.h>
+
+using namespace std;
+int static t[102][1002];
+int knapsack(int w,int wt[],int val[],int n)
+{
+    if(n<0)
+    return 0;
+    if(t[n][w]!=-1)
+    return t[n][w];
+    if(wt[n]>w)
+    {
+        t[n][w]=knapsack(w,wt,val,n-1);
+        return t[n][w];
+    }
+    else
+    {
+        t[n][w]=max(val[n]+knapsack(w-wt[n],wt,val,n-1),knapsack(w,wt,val,n-1));
+        return t[n][w];
+    }
+}
+int main()
+{
+    int n,w;
+    cin>>n>>w;
+    int wt[n],val[n];
+    for(int i=0;i<n;i++)
+    {
+        cin>>wt[i];
+    }
+    for(int i=0;i<n;i++)
+    {
+        cin>>val[i];
+    }
+
+    memset(t,-1,sizeof(t));
+   cout<<knapsack(w,wt,val,n-1);
+    return 0;
+}
+
